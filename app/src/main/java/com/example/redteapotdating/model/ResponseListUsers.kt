0413ea7b08a2951f0944aebf3094bf41f7ec1068
manvitadalal <manvita7@gmail.com>
2023-02-08 02:47:54 -0500

    First commit

--- app/src/main/java/com/example/redteapotdating/model/ResponseListUsers.kt
+++
@@ -1,8 +0,0 @@
-package com.example.redteapotdating.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ResponseListUsers(
-    @SerializedName("users")
-    var users: List<User>
-)
